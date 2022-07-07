e3d28bf288282e6ccfd77582b4f0b5d08eb37e7d
Shibbaz <kmosc@protonmail.com>
2022-07-07 14:06:44 +0200

    new changes

--- app/contexts/transactions/commands/create.rb
+++ app/contexts/transactions/commands/create.rb
@@ -6,14 +6,17 @@ module Contexts
           stream = event.data
 
           receiver_id = stream[:params][:receiver].to_i
-          raise StandardError.new "Amount can't be 0 or below" if (stream[:params][:amount].to_d) <= 0.0
-          raise StandardError.new "Receiver can't be sender" if receiver_id.equal?(stream[:current_user_id])
-          raise StandardError.new "Sent Amount can't be bigger than Balance" if (stream[:balance] - stream[:params][:amount].to_d) <= 0.0
+          current_user_id = stream[:current_user_id]
+          amount = stream[:params][:amount].to_d
+          balance = stream[:balance]
+          raise StandardError.new "Amount can't be 0 or below" if (amount) <= 0.0
+          raise StandardError.new "Receiver can't be sender" if receiver_id.equal?(current_user_id)
+          raise StandardError.new "Amount can't be bigger than Balance" if (balance - amount) <= 0.0
 
           receiver ||= User.find(receiver_id)
           raise ActiveRecord::RecordNotFound if receiver.nil?
 
-          stream[:adapter].create!(stream[:params].merge(sender: stream[:current_user_id]))
+          stream[:adapter].create!(stream[:params].merge(sender: current_user_id))
         end
       end
     end
